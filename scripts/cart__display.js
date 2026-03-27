7366c4f0427e0a63a5d287053ca8f65830a91256
Ehi Ejakhian <ehiejakhian@gmail.com>
2026-03-27 17:23:26 +0100

    I'm tired

--- scripts/cart__display.js
+++ scripts/cart__display.js
@@ -6,7 +6,7 @@ cart_temp = cart_temp.content;
 export function displayCart(name) {
   let cart_data = JSON.parse(localStorage.getItem('SIC-cart-data')) || [];
   let [carts_info, carts] = cart_data;
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////--Continue here
 
   let clone = document.importNode(cart_temp, true);
   //---- Now start ammending stuff----//
